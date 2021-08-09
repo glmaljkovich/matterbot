db92ee17aeeca9806e871d727c36105d09837907
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2021-08-09 20:44:57 -0300

    init

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+define("GIPHY_API_KEY", $_ENV["GIPHY_API_KEY"]);
+define("GIPHY_API_URL", $_ENV["GIPHY_API_URL"]);
+define("GIPHY_LANG", $_ENV["GIPHY_LANG"]);
+define("MATTERMOST_API_KEY", $_ENV["MATTERMOST_API_KEY"]);
+
+// The proxy will not accept GET, PUT or DELETE requests
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+// The proxy will always return JSON responses
+header('Content-Type: application/json');
+
+
+function proxy() {
+    [$command, $search] = parse_command($_POST["command"]);
+    switch ($command) {
+        case 'gif':
+            search($search);
+            break;
+        default:
+            http_response_code(404);
+            break;
+    }
+}
+
+function parse_command($command) {
+    return explode(" ", urldecode($command));
+}
+
+function search($query) {
+    $url = GIPHY_API_URL . "/gifs/search?api_key=" . GIPHY_API_KEY
+                         . "&q=" . $query
+                         . "&limit=10&offset=0&rating=g"
+                         . "&lang=" . GIPHY_LANG;
+    $results = get($url);
+    $gif = select_gif($results);
+    $response = array(
+        'response_type' => 'in_channel',
+        'text' => '![gif](' . $gif . ')',
+        'url' => $url
+    );
+    echo json_encode($response, JSON_UNESCAPED_SLASHES);
+}
+
+function select_gif($response) {
+    return $response["data"][0]["images"]["original"]["url"];
+}
+
+
+// REST helpers
+
+function post($url, $data) {
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    return $result;
+}
+
+function get($url) {
+    return json_decode(file_get_contents($url), true);
+}
+
+proxy();
+
+?>
