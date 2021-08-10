709188cf70728c629a1f473fe454de901726e7c7
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2021-08-09 21:17:36 -0300

    query

--- index.php
+++ index.php
@@ -40,7 +40,8 @@ function search($query) {
 }
 
 function select_gif($response) {
-    return $response["data"][0]["images"]["original"]["url"];
+    $random_result_index = random_int(0, 10);
+    return $response["data"][$random_result_index]["images"]["original"]["url"];
 }
 
 
